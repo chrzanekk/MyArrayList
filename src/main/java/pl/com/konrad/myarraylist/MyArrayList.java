487cf268f5823adecd0645fa1851ccc03777070f
Konrad <chrzanekk@gmail.com>
2021-02-08 19:35:24 +0100

    some changes in basic methods.

--- src/main/java/pl/com/konrad/myarraylist/MyArrayList.java
+++ src/main/java/pl/com/konrad/myarraylist/MyArrayList.java
@@ -4,29 +4,57 @@ import java.util.*;
 import java.util.function.UnaryOperator;
 
 public class MyArrayList implements List {
-    private int listSize = 0;
-    private Object[] list = new Object[listSize];
+    private Node head;
 
 
-//+
+    public MyArrayList() {
+        this.head = null;
+    }
+
+
+    //+
     @Override
     public int size() {
-        return (int) Arrays.stream(list).count();
+        return getSize(head);
     }
-//+
+
+    private static int getSize(Node node) {
+        Node current = node;
+        int sum = 0;
+        if (current == null) {
+            throw new NullPointerException("List is empty.");
+        } else {
+            while (current != null) {
+                sum++;
+                current = current.next;
+            }
+        }
+        return sum;
+    }
+
+    //+
     @Override
     public boolean isEmpty() {
-        return Arrays.stream(list).count() == 0;
+        return head == null;
     }
-//+
+
+    //+
     @Override
     public boolean contains(Object o) {
-        if (listSize!=0) {
-            for (Object object : list) {
-                if (o.equals(object)) {
-                    return true;
-                }
+        if (isEmpty()) {
+            return false;
+        } else {
+            return isContains(o, head);
+        }
+    }
+
+    private static boolean isContains(Object o, Node head) {
+        Node current = head;
+        while (current != null && !current.data.equals(o)) {
+            if (current.data.equals(o)) {
+                return true;
             }
+            current = current.next;
         }
         return false;
     }
@@ -38,26 +66,34 @@ public class MyArrayList implements List {
 
     @Override
     public Object[] toArray() {
-        return Arrays.copyOf(list,list.length);
+        return null;
     }
-//+
+
+    //+
     @Override
     public boolean add(Object o) {
-        list = new Object[listSize+1];
-        list[list.length-1] = o;
+        return addNew(o,head);
+    }
+    private boolean addNew(Object o, Node head) {
+        Node current = head;
+        Node newNode = new Node(o);
+        if (current == null) {
+            this.head = newNode;
+        }
+        else {
+            while(current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
         return true;
     }
-//+
+
+    //+
     @Override
     public boolean remove(Object o) {
-        if (listSize!=0) {
-            for (Object object : list) {
-                if (o.equals(object)) {
-                    
-                    return true;
-                }
-            }
-        }
+
+
         return false;
     }
 
@@ -80,37 +116,44 @@ public class MyArrayList implements List {
     public void sort(Comparator c) {
 
     }
-//+
+
+    //+
     @Override
     public void clear() {
 
     }
-//+
+
+    //+
     @Override
     public Object get(int index) {
         return null;
     }
-//+
+
+    //+
     @Override
     public Object set(int index, Object element) {
         return null;
     }
-//+
+
+    //+
     @Override
     public void add(int index, Object element) {
 
     }
-//+
+
+    //+
     @Override
     public Object remove(int index) {
         return null;
     }
-//+
+
+    //+
     @Override
     public int indexOf(Object o) {
         return 0;
     }
-//+
+
+    //+
     @Override
     public int lastIndexOf(Object o) {
         return 0;
@@ -125,7 +168,8 @@ public class MyArrayList implements List {
     public ListIterator listIterator(int index) {
         return null;
     }
-//+
+
+    //+
     @Override
     public List subList(int fromIndex, int toIndex) {
         return null;
@@ -140,12 +184,14 @@ public class MyArrayList implements List {
     public boolean retainAll(Collection c) {
         return false;
     }
-//+
+
+    //+
     @Override
     public boolean removeAll(Collection c) {
         return false;
     }
-//+
+
+    //+
     @Override
     public boolean containsAll(Collection c) {
         return false;
@@ -154,5 +200,31 @@ public class MyArrayList implements List {
     @Override
     public Object[] toArray(Object[] a) {
         return new Object[0];
+    }
+
+    // nie wiem czy mi się settery przydadzą, czy węzeł jest DTOsem czy nie.
+    class Node {
+        private Node next;
+        private Object data;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Object getData() {
+            return data;
+        }
+
+        public void setData(Object data) {
+            this.data = data;
+        }
     }
 }
